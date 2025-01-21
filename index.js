b91a6ddea6af764126f6ec62abe31f7beb7cd26c
Jose Carbajal <jose.carbajal.salinas@gmail.com>
2025-01-21 13:03:14 -0600

    January21st

--- index.js
+++ index.js
@@ -25,16 +25,16 @@ const { Console } = require('console');
         //await getSchedulesPerYear();
         //await getGamesPerYear();
 
-        // var years = [2024]//, //2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015];//[2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000, 1999];
-        // for (let index = 0; index < years.length; index++) {
-        //     const yearTo = years[index];
-        //     await prepareData(yearTo);
-        //     await formatGamesPerTeam(yearTo);
-        //     await generateAverages(yearTo);
-        // }
+        var years = [2024]//, //2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015];//[2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000, 1999];
+        for (let index = 0; index < years.length; index++) {
+            const yearTo = years[index];
+            await prepareData(yearTo);
+            await formatGamesPerTeam(yearTo);
+            await generateAverages(yearTo);
+        }
 
 
-        //var years = [2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015];
+        // var years = [2024];//[2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015];
         // for (let index = 0; index < years.length; index++) {
         //     const yearTo = years[index];
         //     var toBeEvaluated = false;
@@ -48,12 +48,12 @@ const { Console } = require('console');
         //     await save("MLData", MLData, function () { }, "replace", "AnalysisData")
         // }
 
-        // var years = [2024];
-        // for (let index = 0; index < years.length; index++) {
-        //     const yearTo = years[index];
-        //     var toBeEvaluated = true;
-        //     await generateMLRecords(yearTo, toBeEvaluated);
-        // }
+        var years = [2024];
+        for (let index = 0; index < years.length; index++) {
+            const yearTo = years[index];
+            var toBeEvaluated = true;
+            await generateMLRecords(yearTo, toBeEvaluated);
+        }
 
 
         //await enrichMLResults("2024NewMLResults", 2024, "Week11", 2023);
@@ -1032,7 +1032,7 @@ try{
                     {
                         var stopHere = "";
                     }
-                    if (schedules.length > 0 && isYear != 2024 && isYear == yearToProcess) {
+                    if (schedules.length > 0 && !toBeEvaluated && isYear == yearToProcess) {
                         for (let rat = (schedules.length - 1); rat >= 0; rat--) {
                             const schedule = schedules[rat];
                             var schedule_name = formatDateString(schedule.date_game).replace(" ", "_").replace(" ", "_").replace(",", "");
@@ -1178,7 +1178,7 @@ try{
                                 const schedule = schedules[rat];
                                 const gameDate = new Date(schedule.date_game);
                                 
-                                if(schedule.date_game != "Date" && (gameDate.toDateString() == today.toDateString() || gameDate < today )){
+                                if(schedule.date_game != "Date" && (gameDate.toDateString() == today.toDateString() /* || gameDate < today */ )){
                                 var homePossibleTeam = schedule.box_score_textLink.split(".")[0].slice(-3);
                                 var opponentName = schedule.opp_nameLink.split('/')[2];
                                 school_name = school_name.replace(/\s+/g, '').replace(/\(\d{1,2}\)/, '').replace("_", "");
