c46ad2f8231291ec49e0a8a046843f7583d50d42
Jose Carbajal <jose.carbajal.salinas@gmail.com>
2025-01-31 22:11:28 -0600

    Jan31st

--- index.js
+++ index.js
@@ -25,35 +25,35 @@ const { Console } = require('console');
         //await getSchedulesPerYear();
         //await getGamesPerYear();
 
-        var years = [2024]//, //2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015];//[2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000, 1999];
-        for (let index = 0; index < years.length; index++) {
-            const yearTo = years[index];
-            await prepareData(yearTo);
-            await formatGamesPerTeam(yearTo);
-            await generateAverages(yearTo);
-        }
-
-
-        var years = [2024];//, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015];
-        for (let index = 0; index < years.length; index++) {
-            const yearTo = years[index];
-            var toBeEvaluated = false;
-            await generateMLRecords(yearTo, toBeEvaluated);
-        }
-        // var MLData = [];
+        // var years = [2024]//, //2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015];//[2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000, 1999];
         // for (let index = 0; index < years.length; index++) {
         //     const yearTo = years[index];
-        //     var data = await load(yearTo + "MLData", "AnalysisData")
-        //     MLData = MLData.concat(data);
-        //     await save("MLData", MLData, function () { }, "replace", "AnalysisData")
+        //     await prepareData(yearTo);
+        //     await formatGamesPerTeam(yearTo);
+        //     await generateAverages(yearTo);
         // }
 
-        var years = [2024];
+
+        var years = [2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015];
+        // for (let index = 0; index < years.length; index++) {
+        //     const yearTo = years[index];
+        //     var toBeEvaluated = false;
+        //     await generateMLRecords(yearTo, toBeEvaluated);
+        // }
+        var MLData = [];
         for (let index = 0; index < years.length; index++) {
             const yearTo = years[index];
-            var toBeEvaluated = true;
-            await generateMLRecords(yearTo, toBeEvaluated);
+            var data = await load(yearTo + "MLData", "AnalysisData")
+            MLData = MLData.concat(data);
+            await save("MLData", MLData, function () { }, "replace", "AnalysisData")
         }
+
+        // var years = [2024];
+        // for (let index = 0; index < years.length; index++) {
+        //     const yearTo = years[index];
+        //     var toBeEvaluated = true;
+        //     await generateMLRecords(yearTo, toBeEvaluated);
+        // }
 
         // var MLData2015 = await load("2015MLData", "AnalysisData");
         // var MLData2016 = await load("2016MLData", "AnalysisData");
