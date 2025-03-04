cfa4e1d653a0f7e9f1a5861d7ee6c49d788f23fc
Jose Carbajal <jose.carbajal.salinas@gmail.com>
2025-03-04 10:52:14 -0600

    Mrch 3rd

--- index.js
+++ index.js
@@ -25,16 +25,16 @@ const { Console } = require('console');
         //await getSchedulesPerYear();
         //await getGamesPerYear();
 
-        var years = [2024];//, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015];//[2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000, 1999];
-        for (let index = 0; index < years.length; index++) {
-            const yearTo = years[index];
-            await prepareData(yearTo);
-            await formatGamesPerTeam(yearTo);
-            await generateAverages(yearTo);
-        }
+        // var years = [2024];//, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015];//[2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000, 1999];
+        // for (let index = 0; index < years.length; index++) {
+        //     const yearTo = years[index];
+        //     await prepareData(yearTo);
+        //     await formatGamesPerTeam(yearTo);
+        //     await generateAverages(yearTo);
+        // }
 
 
-        var years = [2024];//, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015];
+        var years = [2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015];
         // for (let index = 0; index < years.length; index++) {
         //     const yearTo = years[index];
         //     var toBeEvaluated = false;
