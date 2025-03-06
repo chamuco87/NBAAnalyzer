8b6d5567970c675acd32ac54757f1abedd654d04
Jose Carbajal <jose.carbajal.salinas@gmail.com>
2025-03-05 18:10:56 -0600

    March 5th

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const { Console } = require('console');
         //await getTableData("/leagues/" ,"stats", "BaseData");
         //await getSeasonsPerYear();
         //await getSchedulesPerYear();
-        await getGamesPerYear();
+        //await getGamesPerYear();
 
         // var years = [2024];//, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015];//[2024, 2023, 2022, 2021, 2020, 2019, 2018, 2017, 2016, 2015, 2014, 2013, 2012, 2011, 2010, 2009, 2008, 2007, 2006, 2005, 2004, 2003, 2002, 2001, 2000, 1999];
         // for (let index = 0; index < years.length; index++) {
